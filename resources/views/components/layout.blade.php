ef794a67120ae5f0d24bd4e36b2e7c23c8a496cc
Angelito Forbes <angelitoforbes08161995@gmail.com>
2025-09-16 16:56:58 +0800

    jnt return scanned

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Home Page</title>
+  <title>{{ $title ?? 'Home Page' }}</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://unpkg.com/alpinejs" defer></script>
 </head>
