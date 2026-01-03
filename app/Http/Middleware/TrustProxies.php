91938c9dc882327ceeda21cbd51ea3214abc42c8
Angelito Forbes <angelitoforbes08161995@gmail.com>
2026-01-04 04:31:02 +0800

    Fix TrustProxies + remove stray char

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,10 +7,8 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    // Trust all proxies (Heroku routers / reverse proxies)
     protected $proxies = '*';
 
-    // Use forwarded headers so request()->ip() becomes the real client IP
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
