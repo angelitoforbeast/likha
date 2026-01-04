095635079f70d44d3a85e97f516d65665157283d
Angelito Forbes <angelitoforbes08161995@gmail.com>
2026-01-04 15:02:52 +0800

    allowedips only

--- resources/views/ads_manager/pancake-subscription-checker.blade.php
+++ resources/views/ads_manager/pancake-subscription-checker.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  <x-slot name="heading">Pancake Subscription Checkers</x-slot>
+  <x-slot name="heading">Pancake Subscription Checkers 010426 1502</x-slot>
 
   {{-- Flatpickr styles --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
