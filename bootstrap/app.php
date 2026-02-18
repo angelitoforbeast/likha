60385a58f5325d2867bdade14f0f38c0cb804f8d
Angelito Forbes <angelitoforbes08161995@gmail.com>
2026-02-18 23:42:34 +0800

    Adjsut limit of item name to 50 characters 02152026 0322PM

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,19 +7,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    // ✅ Trust proxy headers (Heroku / reverse proxies)
-    $middleware->trustProxies(at: '*');
+        // ✅ Trust proxy headers (Heroku / reverse proxies)
+        $middleware->trustProxies(at: '*');
 
-    // ✅ Ensure alias exists (safe kahit meron na sa Kernel)
-    $middleware->alias([
-        'allowed_ip' => \App\Http\Middleware\AllowedIpMiddleware::class,
-    ]);
-})
-
+        // ✅ Ensure alias exists (safe kahit meron na sa Kernel)
+        $middleware->alias([
+            'allowed_ip' => \App\Http\Middleware\AllowedIpMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
