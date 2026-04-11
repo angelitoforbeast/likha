40730ef87990e1c3e415654dc70de7caf2a66dca
Angelito Forbes <angelitoforbes08161995@gmail.com>
2026-04-11 21:55:29 +0800

    chore: remove debug log from analyzeSubmission

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -282,8 +282,6 @@ class ChecklistController extends Controller
 
     public function analyzeSubmission(Request $request, ChecklistSubmission $submission)
     {
-        \Log::info('ANALYZE_HIT', ['submission_id' => $submission->id, 'user' => auth()->id()]);
-
         $submission->load(['task', 'files']);
         $task     = $submission->task;
         $imgFiles = $submission->files->filter(fn($f) => $f->isImage());
